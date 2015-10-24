fbfc9cd75f4e898634d68a9fa157cd46ac6e31b1
rafael-santos98 <rafaelsemprerock@gmail.com>
2015-10-24 16:29:47 -0200

    24-10-2015

---
+++ SF_AP/oProdutoMovimento.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Data;
+using System.Collections.Generic;
+
+namespace SF_AP
+{
+    public class oProdutoMovimento : SF_BLL.oProduto
+    {
+        public DataTable CarregaProdutoMovimento(int ncdProdutoMovimento, Nullable<DateTime> dtMovimentoDe, Nullable<DateTime> dtMovimentoAte, int ncdProduto, string cdsTipoMovimento, string ConnectionString)
+        {
+            try
+            {
+                SF_BLL.oProdutoMovimento oMetodo = new SF_BLL.oProdutoMovimento();
+                DataTable dt = new DataTable();
+
+                dt = oMetodo.CarregaProdutoMovimento(ncdProdutoMovimento, dtMovimentoDe, dtMovimentoAte, ncdProduto, cdsTipoMovimento, ConnectionString);
+
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }   
+
+        public DataTable IncluiProdutoMovimento(int ncdProduto, float nvlQuantidade, string cdsObservacao, string cdsTipoMovimento, string ConnectionString)
+        {
+            try
+            {
+                SF_BLL.oProdutoMovimento oMetodo = new SF_BLL.oProdutoMovimento();
+                DataTable dt = new DataTable();
+
+                dt = oMetodo.IncluiProdutoMovimento(ncdProduto, nvlQuantidade, cdsObservacao, cdsTipoMovimento, ConnectionString);
+
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
